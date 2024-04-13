19040bfe4aa1d61b394fc2f2f10af656cbf02bbd
Leandro Andrade <leandrade666@gmail.com>
2024-04-13 15:09:15 -0300

    Criação da classe base Veículo e das classes derivadas: Moto Caminhão e Triciclo e o método Mostrar VelocidadeMáxima um exeplo de herança e poliformismo

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+// Criação da classd Base Veiculo
+public class Veiculo
+{
+    public virtual void MostrarVelocidadeMaxima()
+        {
+            Console.WriteLine("Velocidade Máxima não definida");
+          
+        }
+    
+}
+
+// Criação da classe Devrivada Moto que herda 
+// o método MostrarVelocidadeMaxima da classe Base Veiculo
+// que tem seu comportamento alterado ou diferente
+// da classe base.
+public class Moto : Veiculo
+{
+    public override void MostrarVelocidadeMaxima()
+    {
+        Console.WriteLine("Velocidade Máxima 150km/h");
+    }
+}
+
+
+
+// Criação da classe Devrivada Caminhão que herda 
+// o método MostrarVelocidadeMaxima da classe Base Veiculo
+// onde tem seu comportamento alterado ou diferente
+// da classe base.
+public class Caminhao : Veiculo
+{
+    public override void MostrarVelocidadeMaxima()
+    {
+        Console.WriteLine("Velocidade Máxima 120Km/h");
+    }
+}
+
+
+// Criação da classe Devrivada Triciclo que herda 
+// o método MostrarVelocidadeMaxima da classe Base Veiculo
+// onde tem seu comportamento alterado ou diferente
+// da classe base.
+
+public class Triciclo : Veiculo
+{
+    public override void MostrarVelocidadeMaxima()
+    {
+        Console.WriteLine("Velocidade máxima 100Km/h");
+    }
+}
+
+
+ public class Program
+{
+
+  public static void Main(string[] args)
+ {
+    // Crição de Objetos das Classes Derivadas
+
+    Moto Susuki = new Moto();
+    Caminhao Scania = new Caminhao();
+    Triciclo triciclo1 = new Triciclo();
+
+    // Chamadas do Métodos  MostrarVelocidadeMaxima de cada objeto"
+     
+    Susuki.MostrarVelocidadeMaxima();
+    Scania.MostrarVelocidadeMaxima();
+    triciclo1.MostrarVelocidadeMaxima();
+    
+ }
+
+}
